72b27d6fcdb595b44ee22eadbca311ed36434c4e
mhersent <melvin.hersent@gmail.com>
2025-06-03 11:07:50 -0400

    fix: linting

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,44 +13,77 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(
-            CarMake.objects.create(name=data['name'], description=data['description'])
+            CarMake.objects.create(name=data['name'],
+            description=data['description'])
         )
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
-      {"name": "Pathfinder", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[0], "motor_type": "INLINE"
-      },
-      {"name": "Qashqai", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[0], "motor_type": "INLINE"},
-      {"name": "XTRAIL", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[0], "motor_type": "INLINE"},
-      {"name": "A-Class", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[1], "motor_type": "INLINE"},
-      {"name": "C-Class", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[1], "motor_type": "INLINE"},
-      {"name": "E-Class", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[1], "motor_type": "INLINE"},
-      {"name": "A4", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[2], "motor_type": "INLINE"},
-      {"name": "A5", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[2], "motor_type": "INLINE"},
-      {"name": "A6", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[2], "motor_type": "INLINE"},
-      {"name": "Sorrento", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[3], "motor_type": "INLINE"},
-      {"name": "Carnival", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[3], "motor_type": "INLINE"},
-      {"name": "Cerato", "dealer_id": 1, "type": "Sedan", "year": 2023,
-        "car_make": car_make_instances[3], "motor_type": "INLINE"},
-      {"name": "Corolla", "dealer_id": 1, "type": "Sedan", "year": 2023,
-        "car_make": car_make_instances[4], "motor_type": "INLINE"},
-      {"name": "Camry", "dealer_id": 1, "type": "Sedan", "year": 2023,
-        "car_make": car_make_instances[4], "motor_type": "INLINE"},
-      {"name": "Kluger", "dealer_id": 1, "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[4], "motor_type": "INLINE"},
+        {
+            "name": "Pathfinder", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[0], "motor_type": "INLINE"
+        },
+        {
+            "name": "Qashqai", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[0], "motor_type": "INLINE"
+        },
+        {
+            "name": "XTRAIL", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[0], "motor_type": "INLINE"
+        },
+        {
+            "name": "A-Class", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[1], "motor_type": "INLINE"
+        },
+        {
+            "name": "C-Class", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[1], "motor_type": "INLINE"
+        },
+        {
+            "name": "E-Class", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[1], "motor_type": "INLINE"
+        },
+        {
+            "name": "A4", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[2], "motor_type": "INLINE"
+        },
+        {
+            "name": "A5", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[2], "motor_type": "INLINE"
+        },
+        {
+            "name": "A6", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[2], "motor_type": "INLINE"
+        },
+        {
+            "name": "Sorrento", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[3], "motor_type": "INLINE"
+        },
+        {
+            "name": "Carnival", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[3], "motor_type": "INLINE"
+        },
+        {
+            "name": "Cerato", "dealer_id": 1, "type": "Sedan", "year": 2023,
+            "car_make": car_make_instances[3], "motor_type": "INLINE"
+        },
+        {
+            "name": "Corolla", "dealer_id": 1, "type": "Sedan", "year": 2023,
+            "car_make": car_make_instances[4], "motor_type": "INLINE"
+        },
+        {
+            "name": "Camry", "dealer_id": 1, "type": "Sedan", "year": 2023,
+            "car_make": car_make_instances[4], "motor_type": "INLINE"
+        },
+        {
+            "name": "Kluger", "dealer_id": 1, "type": "SUV", "year": 2023,
+            "car_make": car_make_instances[4], "motor_type": "INLINE"
+        },
         # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+        CarModel.objects.create(
+            name=data['name'], car_make=data['car_make'],
+            type=data['type'], year=data['year']
+        )
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -57,7 +57,7 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except Exception as err:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
@@ -91,9 +91,10 @@ def get_cars(request):
     return JsonResponse({"CarModels": cars})
 
 
-# Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+# Update the `get_dealerships` render list of dealerships all by default,
+# particular state if state is passed
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if (state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
@@ -103,7 +104,7 @@ def get_dealerships(request, state="All"):
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
-    if(dealer_id):
+    if (dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
@@ -114,21 +115,24 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
+
 def get_dealer_details(request, dealer_id):
-    if(dealer_id):
+    if (dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
+
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
             return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+        except Exception as err:
+            return JsonResponse(
+                {"status": 401, "message": "Error in posting review"}
+            )
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
