c8a3a1a1d398529444e0c1b1e6a9eb52f0a7d3f4
mhersent <melvin.hersent@gmail.com>
2025-06-03 11:17:09 -0400

    fix: linting

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,9 +28,14 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://melvinhersen-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://melvinhersen-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-
+ALLOWED_HOSTS = [
+    'localhost',
+    """https://melvinhersen-8000.theiadockernext-1-
+    labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"""
+]
+CSRF_TRUSTED_ORIGINS = [
+    """https://melvinhersen-8000.theiadockernext-1
+    -labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"""
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
@@ -94,7 +99,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        """django.contrib.auth.password_validation.
+        UserAttributeSimilarityValidator""",
     },
     {
         'NAME':
